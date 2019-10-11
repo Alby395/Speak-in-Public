f19b0cd0390fccb5c7d4c98eda2436a914f7e4e9
Emanuele Torelli <mennetorelli@hotmail.it>
2019-10-11 15:35:31 +0200

    logica per cambiare layer dell'animator

---
+++ Speak in Public/Assets/Scripts/ClassroomManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClassroomManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Animator[] animators = transform.GetComponentsInChildren<Animator>();
+        foreach (Animator animator in animators)
+        {
+            animator.SetLayerWeight(GameObject.Find("Settings").GetComponent<Settings>().ClassroomDifficulty, 1);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
